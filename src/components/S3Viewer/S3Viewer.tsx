cdb1675e994030fc5556b0d542bcc72512f2019a
wenhwang97 <wenhwang@bu.edu>
2023-06-01 17:17:52 -0400

    init parameters for entry point

--- src/components/S3Viewer/S3Viewer.tsx
+++ src/components/S3Viewer/S3Viewer.tsx
@@ -1,5 +1,35 @@
 import { FC } from 'react';
+import { FileMain } from './FileMain';
+import { S3Client } from '@aws-sdk/client-s3';
 
-export const S3Viewer: FC = () => {
-  return <p>Hello World</p>;
+interface S3ViewerProps {
+  client: S3Client;
+  bucket: string;
+  disableActions?: boolean;
+  disableRead?: boolean;
+  disableWrite?: boolean;
+  disableUpload?: boolean;
+  disablePreview?: boolean;
+  disableDelete?: boolean;
+  disableDownload?: boolean;
+  disableRename?: boolean;
+  disableCreateFolder?: boolean;
+}
+
+export const S3Viewer: FC<S3ViewerProps> = (props) => {
+  const action = !props.disableActions;
+  const read = !props.disableRead && action;
+  const write = !props.disableWrite && action;
+
+  const permissions = {
+    actions: action,
+    upload: !props.disableUpload && write,
+    preview: !props.disablePreview && read,
+    delete: !props.disableDelete && action,
+    download: !props.disableDownload && read,
+    rename: !props.disableRename && write,
+    createFolder: !props.disableCreateFolder && write
+  };
+
+  return <FileMain client={props.client} bucket={props.bucket} permissions={permissions} />;
 };
