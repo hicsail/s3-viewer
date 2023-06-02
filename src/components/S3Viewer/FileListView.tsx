587019157800185fb486cd0d45e260dd8c384f9f
wenhwang97 <wenhwang@bu.edu>
2023-06-02 11:30:47 -0400

    add pagination to list view

--- src/components/S3Viewer/FileListView.tsx
+++ src/components/S3Viewer/FileListView.tsx
@@ -1,6 +1,6 @@
-import { FC, MouseEvent, useEffect, useState } from 'react';
+import { FC, MouseEvent, useEffect, useMemo, useState } from 'react';
 import { S3Object } from '../../types/S3Object';
-import { Table, TableBody, TableContainer } from '@mui/material';
+import { Table, TableBody, TableContainer, TablePagination } from '@mui/material';
 import EnhancedTableHead from '../EnhancedTableHead';
 import { ObjectRow } from './ObjectRow';
 
@@ -18,6 +18,12 @@ export const FileListView: FC<FileListViewProps> = (props) => {
   const [objects, setObjects] = useState<S3Object[]>(props.objects);
   const [order, setOrder] = useState<'asc' | 'desc'>('asc');
   const [orderBy, setOrderBy] = useState<string>('name');
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+
+  const visibleRows = useMemo(() => {
+    return objects.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  }, [objects, page, rowsPerPage]);
 
   const columns = [
     { label: 'Name', id: 'name' },
@@ -36,6 +42,17 @@ export const FileListView: FC<FileListViewProps> = (props) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
+  };
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    if (event) {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    }
   };
 
   // update the objects when the props change
@@ -65,15 +82,26 @@ export const FileListView: FC<FileListViewProps> = (props) => {
   }, [order, orderBy]);
 
   return (
-    <TableContainer>
-      <Table>
-        <EnhancedTableHead onRequestSort={handleRequestSort} columns={columns} sortableIds={['name', 'owner', 'date', 'size']} order={order} orderBy={orderBy} />
-        <TableBody>
-          {objects.map((object) => (
-            <ObjectRow key={object.location + object.name} object={object} permissions={props.permissions} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer>
+        <Table>
+          <EnhancedTableHead onRequestSort={handleRequestSort} columns={columns} sortableIds={['name', 'owner', 'date', 'size']} order={order} orderBy={orderBy} />
+          <TableBody>
+            {visibleRows.map((object) => (
+              <ObjectRow key={object.location + object.name} object={object} permissions={props.permissions} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, { label: 'All', value: -1 }]}
+        component="div"
+        count={objects.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
   );
 };
