bc2b014bc9166067b8e3d8fd8e9f1482367e629f
cbolles <collinbolles@gmail.com>
2023-06-14 10:21:12 -0400

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// Components
 export * from './components/S3Viewer/S3Viewer';
+
+//// Plugin Components
+export * from './components/Plugin/PluginView';
+export * from './components/Plugin/DocViewer';
+
+// Types
+export * from './types/Plugin';
