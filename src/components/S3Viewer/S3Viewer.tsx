e03ac4454c4f6eb1511ad01c798a7e4c4d0847c2
cbolles <collinbolles@gmail.com>
2023-06-15 11:30:29 -0400

    Allow user to provide means to control the path

--- src/components/S3Viewer/S3Viewer.tsx
+++ src/components/S3Viewer/S3Viewer.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { Dispatch, FC, SetStateAction, useState } from 'react';
 import { FileMain } from './FileMain';
 import { S3Client } from '@aws-sdk/client-s3';
 import { S3Provider } from '../../contexts/s3-context';
@@ -8,6 +8,10 @@ import { Plugin } from './../../types/Plugin';
 interface S3ViewerProps {
   client: S3Client;
   bucket: string;
+  pathControl?: {
+    setCurrentPath: Dispatch<SetStateAction<string>>,
+    currentPath: string
+  },
   plugins?: Plugin[];
   bucketDisplayedName?: string;
   onCurrentPathChange?: (currentPath: string) => void;
@@ -37,8 +41,20 @@ export const S3Viewer: FC<S3ViewerProps> = (props) => {
     createFolder: !props.disableCreateFolder && write
   };
 
+  // By default, have a useState for the current path
+  let [currentPath, setCurrentPath] = useState<string>('');
+  // Otherwise use the state provided
+  if (props.pathControl) {
+    currentPath = props.pathControl.currentPath;
+    setCurrentPath = props.pathControl.setCurrentPath;
+  }
+
   return (
-    <S3Provider client={props.client} bucket={props.bucket}>
+    <S3Provider
+      client={props.client}
+      bucket={props.bucket}
+      currentPath={currentPath}
+      setCurrentPath={setCurrentPath}>
       <PluginManagerProvider plugins={props.plugins || []}>
         <FileMain
           client={props.client}
