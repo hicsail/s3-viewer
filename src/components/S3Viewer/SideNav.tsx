e9a379d4bf52950ba62dac100fd34844f6d36a91
wenhwang97 <wenhwang@bu.edu>
2023-08-22 16:51:04 -0400

    pass height value to tab content

--- src/components/S3Viewer/SideNav.tsx
+++ src/components/S3Viewer/SideNav.tsx
@@ -24,8 +24,8 @@ const TabPanel: FC<TabPanelProps> = (props) => {
   const { children, value, index, ...other } = props;
 
   return (
-    <Box component="div" role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} {...other}>
-      {value === index && <Box>{children}</Box>}
+    <Box component="div" role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} height="inherit" {...other}>
+      {value === index && <Box height="inherit">{children}</Box>}
     </Box>
   );
 };
